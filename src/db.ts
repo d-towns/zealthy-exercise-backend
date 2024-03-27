82b830a303cb20166654f527f405357abefd2558
dt0wns <dennis@dennistowns.com>
2024-03-27 14:41:46 -0400

    env sampel and env vars

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_URL ?? '', {
+export const sequelize = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.PROD_DB_URL : process.env.POSTGRESS_URL, {
     dialect: "postgres",
     logging: false,
 });
